2757bac522b06b6ee273699a947de8f7ab81a0bd
DoneshD <donesh.dava@gmail.com>
2025-10-13 20:53:31 -0500

    Adding phone subsystem

---
+++ JobHuntSimulator/Private/Phone/PhoneSubsystem.cpp
@@ -0,0 +1,1 @@
+#include "Phone/PhoneSubsystem.h"
---
+++ JobHuntSimulator/Public/Phone/PhoneSubsystem.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "PhoneSubsystem.generated.h"
+
+UCLASS()
+class JOBHUNTSIMULATOR_API UPhoneSubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+};
